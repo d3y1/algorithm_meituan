388d11e6ab62d706095fad07a1c8caa713b74407
d3y1 <d3y1_2u@163.com>
2024-01-28 17:10:19 +0800

    :art: MT7 字符编码: 优先队列: 哈夫曼编码

---
+++ src/main/java/simple/MT7.java
@@ -0,0 +1,48 @@
+package simple;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * MT7 字符编码
+ * @author d3y1
+ */
+public class MT7{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNext()) {
+            solution(in);
+        }
+    }
+
+    /**
+     * 优先队列: 哈夫曼编码
+     * @param in
+     */
+    private static void solution(Scanner in){
+        String str = in.nextLine();
+
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        for(char ch: str.toCharArray()){
+            charCountMap.put(ch, charCountMap.getOrDefault(ch, 0)+1);
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int value : charCountMap.values()){
+            queue.offer(value);
+        }
+
+        int result = 0;
+        while(queue.size() >= 2) {
+            int a = queue.poll();
+            int b = queue.poll();
+            result += a+b;
+            queue.offer(a+b);
+        }
+
+        System.out.println(result);
+    }
+}
